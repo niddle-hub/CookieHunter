e45939afffcb034c5454af81e09dbd3147a78145
Daniil Aleksandrov <aleksandrovdd@vk.com>
2022-03-21 21:43:26 +0300

    Initial commit

---
+++ Ue4/CookieHunter/Source/CookieHunter/CookieHunter.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CookieHunter.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CookieHunter, "CookieHunter" );
+ 
